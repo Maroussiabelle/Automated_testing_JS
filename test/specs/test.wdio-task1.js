1f25c0c10cbcf8e643764b55c72d4c2bc811e4d7
Marija Voroncova <marijagrigule@gmail.com>
2024-05-02 11:41:04 +0300

    Task 1 updated solution

--- test/specs/test.wdio-task1.js
+++ test/specs/test.wdio-task1.js
@@ -1,20 +1,23 @@
 import { $, browser } from '@wdio/globals'
 
-describe('WebDriver suite', () => {
-  it('Task 1 test', async () => {
+describe('WebDriver Task 1 suite', () => {
+  it('Should open webpage and create new paste with given attributes', async () => {
     await browser.url(
       'https://pastebin.com/'
     )
+    // wait for "We value your privacy" window to appear and press "agree" button
     await browser.pause(2000)
-
     const agreeButton = await $('button[mode="primary"]')
     await agreeButton.click()
+    // Write code: "Hello from WebDriver"
     const textArea = await $('textarea#postform-text')
     await textArea.setValue('Hello from WebDriver')
+    // Paste Expiration: "10 Minutes"
     const pasteExpirationSelectSpan = await $('span#select2-postform-expiration-container')
     await pasteExpirationSelectSpan.click()
     const option10M = await $("//li[text()='10 Minutes']")
     await option10M.click()
+    // Paste Name / Title: "helloweb"
     const pasteNameTitle = await $('input#postform-name')
     await pasteNameTitle.setValue('helloweb')
     const createNewPasteButton = await $('button[type="submit"]')
