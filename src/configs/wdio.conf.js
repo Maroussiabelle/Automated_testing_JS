8984e16d75253d606688483cb5d7e82efae9b325
Marija Voroncova <marijagrigule@gmail.com>
2024-05-06 20:03:15 +0300

    Changed structure of project, created config folder, updated packaje.json and wdio.conf.js accordingly

--- src/configs/wdio.conf.js
+++ src/configs/wdio.conf.js
@@ -22,7 +22,7 @@ export const config = {
   //
   specs: [
     // './test/specs/**/*.js'
-    './test/specs/test.wdio-task2.js'
+    '../test/specs/test.wdio-task2.js'
   ],
   // Patterns to exclude.
   exclude: [
@@ -61,7 +61,7 @@ export const config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: 'error',
   //
   // Set specific log levels per logger
   // loggers:
@@ -85,7 +85,7 @@ export const config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  // baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8080',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
