49560ddfb45bbccd1133ebee9a6522fd4d127b8e
Marija Voroncova <marijagrigule@gmail.com>
2024-05-08 00:39:43 +0300

    task3 solution without page components

--- src/test/specs/test.wdio-task3.js
+++ src/test/specs/test.wdio-task3.js
@@ -1,59 +1,114 @@
-import { $, browser } from '@wdio/globals'
+import {$, browser} from '@wdio/globals'
 import StartPage from '../../google_cloud/pages/startPage.js'
+import {expect} from "chai";
 
 const googleStartPage = new StartPage()
 
-async function setMachineType (model) {
-  const selectMachineDropdown = $('div[jsname="kgDJk"]')
-  await selectMachineDropdown.scrollIntoView({ block: 'center' })
-  await selectMachineDropdown.click()
-  await $(`li[data-value='${model}']`).click()
+async function clickSearchIcon() {
+    await $('input.mb2a7b').click()
 }
 
-async function clickGPUSwitch () {
-  const addGPUsSwitch = $('button[aria-label="Add GPUs"]')
-  await addGPUsSwitch.scrollIntoView({ block: 'center' })
-  await addGPUsSwitch.click()
+async function waitForSearchInputFieldDisplayed() {
+    await $('input[name="q"]').waitForDisplayed()
+
 }
-
-describe('WebDriver Task 3 suite', () => {
-  beforeEach(async () => {
-    await googleStartPage.open()
-  })
-
-  it('Should open Google Cloud, search for "Google Cloud Platform Pricing Calculator, set properties and send price estimate to email', async () => {
-    const searchIcon = await $('input.mb2a7b')
-    await searchIcon.click()
-
-    // Wait for the search input field to become visible and interactable
-    const searchInput = await $('input[name="q"]')
-    await searchInput.waitForDisplayed()
-
-    // Enter "Google Cloud Platform Pricing Calculator" into the search field
-    await searchInput.setValue('Google Cloud Platform Pricing Calculator')
-
-    // Press Enter key
+async function searchforItem(searchItem) {
+    await $('input[name="q"]').setValue(searchItem)
     await browser.keys(['Enter'])
-    await $('div.gs-title > a.gs-title > b').click()
+}
+
+async function clickFirstSearchResult() {
+    await $('//a[@class="gs-title"]/b[text()="Google Cloud Pricing Calculator"]').click()
+}
+
+async function clickAddToEstimateButton() {
     await $('span.UywwFc-vQzf8d[jsname="V67aGc"]').click()
-    await $('h2.honxjf').click()
-    await $('input[jsname="YPqjbf"]').click()
-    await browser.keys(['Backspace', '4'])
-    // // await browser.scroll(0, 700)
-    await setMachineType('n1-standard-8')
-    await clickGPUSwitch()
+}
 
-    // await browser.debug()
-    // const addGPUsSwitch = $('button[aria-label="Add GPUs"]')
-    // await addGPUsSwitch.scrollIntoView({ block: 'center' })
-    // await addGPUsSwitch.click()
+async function clickComputeEngine() {
+    await $('//h2[text()="Compute Engine"]').click()
+}
 
-    // await browser.scroll(0, 1700)
-    // await $('span[jscontroller="QjUiqc"][jsname="B9mpmd"]').click()
-    // await $('span[aria-hidden="true"][role="listbox"][id="c9391"][style="display: none;"]').click()
+async function setNumberOfInstances(numberOfInstances) {
+    await $('//div[contains(text(), "Number of Instances") and string-length() > 19]/ancestor::div[@class="QiFlid"]//input[@type="number"]')
+        .click()
+    await browser.keys(['Backspace', `${numberOfInstances}`])
+}
 
-    // await $('.glue-cookie-notification-bar__accept').click()
+async function setMachineType(model) {
+    const selectMachineDropdown = $('div[jsname="kgDJk"]')
+    await selectMachineDropdown.scrollIntoView({block: 'center'})
+    await selectMachineDropdown.click()
+    await $(`li[data-value='${model}']`).click()
+}
 
-    // await browser.pause(5000)
-  })
+async function clickGPUSwitch() {
+    const addGPUsSwitch = $('button[aria-label="Add GPUs"]')
+    await addGPUsSwitch.scrollIntoView({block: 'center'})
+    await addGPUsSwitch.click()
+}
+
+async function selectGPUModel(gpuModel) {
+    const selectGPU = $('//span[text()="GPU Model"]/ancestor::div[contains(@class, "O1htCb-H9tDt")]')
+    await selectGPU.click()
+    await $(`//span[@jsname="K4r5Ff" and text()="${gpuModel}"]/ancestor::li`).click()
+}
+
+async function selectRegion(region) {
+    const selectGPU = $('//span[text()="Region"]/ancestor::div[contains(@class, "O1htCb-H9tDt")]')
+    await selectGPU.click()
+    await $(`//span[text()="${region}"]/ancestor::li`).click()
+}
+
+async function commitedUsage(period){
+    await $(`label.zT2df[for="${period}"]`).click()
+}
+
+async function clickShare(){
+    const shareButton = await $('span[jsname="V67aGc"].FOBRw-vQzf8d')
+    await shareButton.scrollIntoView({block: 'center'})
+    await shareButton.click()
+}
+
+async function openEstimateSummaryInNewTab() {
+    await $('//a[text()="Open estimate summary"]').click()
+}
+describe('WebDriver Task 3 suite', () => {
+    beforeEach(async () => {
+        await googleStartPage.open()
+    })
+
+    it('Should open Google Cloud, search for "Google Cloud Platform Pricing Calculator, set properties and send price estimate to email', async () => {
+
+        await clickSearchIcon()
+        await waitForSearchInputFieldDisplayed()
+        await searchforItem('Google Cloud Platform Pricing Calculator')
+        await clickFirstSearchResult()
+        await clickAddToEstimateButton()
+        await clickComputeEngine()
+        await setNumberOfInstances(4)
+        await setMachineType('n1-standard-8')
+        await clickGPUSwitch()
+        await selectGPUModel('NVIDIA Tesla V100')
+        await selectRegion('Netherlands (europe-west4)')
+        await commitedUsage('1-year')
+        await browser.pause(1000)
+        const sumOnFormTab = await $('.gt0C8e.MyvX5d.D0aEmf').getHTML(false)
+        await clickShare()
+        await openEstimateSummaryInNewTab()
+
+        await browser.switchWindow('Google Cloud Estimate Summary')
+
+        const sumOnSummaryTab = await $('.n8xu5.Nh2Phe.D0aEmf').getHTML(false)
+        const numberOfInstances = await $('//span[text()="Number of Instances"]/parent::span/span[@class="Kfvdz"]')
+            .getHTML(false)
+        const machineType = await $('//span[text()="Machine type"]/parent::span/span[@class="Kfvdz"]')
+            .getHTML(false)
+        const gpuModel = await $('//span[text()="GPU Model"]/parent::span/span[@class="Kfvdz"]')
+            .getHTML(false)
+        expect(sumOnFormTab).to.equal(sumOnSummaryTab)
+        expect(numberOfInstances).to.equal('4')
+        expect(machineType.startsWith('n1-standard-8')).to.be.true
+        expect(gpuModel).to.equal('NVIDIA Tesla V100')
+    })
 })
