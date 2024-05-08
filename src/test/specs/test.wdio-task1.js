84b898b4ef3720d38f143a2028826e4c65b7e591
Marija Voroncova <marijagrigule@gmail.com>
2024-05-08 18:35:36 +0300

    stabilized task 1 solution

--- src/test/specs/test.wdio-task1.js
+++ src/test/specs/test.wdio-task1.js
@@ -1,18 +1,24 @@
 import { $, browser } from '@wdio/globals'
 
+async function tryCloseGoogleAd(){
+  try {
+    await $('vli.vliIgnore').click()
+  } catch(ignored) {
+  }
+}
+
 describe('WebDriver Task 1 suite', () => {
   it('Should open webpage and create new paste with given attributes', async () => {
     await browser.url(
       'https://pastebin.com/'
     )
-    // wait for "We value your privacy" window to appear and press "agree" button
-    await browser.pause(2000)
-    const agreeButton = await $('button[mode="primary"]')
-    await agreeButton.click()
+    await $('div#qc-cmp2-ui button[mode="primary"]').click()
+    await tryCloseGoogleAd()
     // Write code: "Hello from WebDriver"
     const textArea = await $('textarea#postform-text')
     await textArea.setValue('Hello from WebDriver')
     // Paste Expiration: "10 Minutes"
+    await browser.execute(() => window.scrollTo({top: 500, behavior: 'smooth'}))
     const pasteExpirationSelectSpan = await $('span#select2-postform-expiration-container')
     await pasteExpirationSelectSpan.click()
     const option10M = await $("//li[text()='10 Minutes']")
